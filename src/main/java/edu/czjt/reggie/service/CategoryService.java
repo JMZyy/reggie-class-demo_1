e63666e79d76ea13bfa49913332f252c634989e6
YH011229 <136041942+YH011229@users.noreply.github.com>
2023-06-16 08:35:14 +0800

    Update CategoryService.java

--- src/main/java/edu/czjt/reggie/service/CategoryService.java
+++ src/main/java/edu/czjt/reggie/service/CategoryService.java
@@ -3,8 +3,5 @@ package edu.czjt.reggie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.entity.Category;
 
-/**
- * Created by jinkun.tian on 2023/4/27
- */
 public interface CategoryService extends IService<Category> {
 }
