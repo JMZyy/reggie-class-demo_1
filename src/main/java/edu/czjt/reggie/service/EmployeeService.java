c9e5accaf26f04bcf3657c4c77d84c303f6d8c15
YH011229 <136041942+YH011229@users.noreply.github.com>
2023-06-16 08:35:58 +0800

    Update EmployeeService.java

--- src/main/java/edu/czjt/reggie/service/EmployeeService.java
+++ src/main/java/edu/czjt/reggie/service/EmployeeService.java
@@ -2,9 +2,5 @@ package edu.czjt.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.entity.Employee;
-
-/**
- * Created by jinkun.tian on 2023/3/16
- */
 public interface EmployeeService extends IService<Employee> {
 }
