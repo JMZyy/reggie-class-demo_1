fd44b1779948d231794b8567067622a177d3caad
YH011229 <136041942+YH011229@users.noreply.github.com>
2023-06-16 08:35:34 +0800

    Update DishFlavorService.java

--- src/main/java/edu/czjt/reggie/service/DishFlavorService.java
+++ src/main/java/edu/czjt/reggie/service/DishFlavorService.java
@@ -4,10 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.entity.DishFlavor;
 
 import java.util.List;
-
-/**
- * Created by jinkun.tian on 2023/4/27
- */
 public interface DishFlavorService extends IService<DishFlavor> {
 
     public List<DishFlavor> getFlavorsByDishId(Long dishId);
