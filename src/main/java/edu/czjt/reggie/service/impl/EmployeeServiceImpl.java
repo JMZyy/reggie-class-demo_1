a5953dce6efb6bc0481b8976eb4a9f8380549f1b
YH011229 <136041942+YH011229@users.noreply.github.com>
2023-06-16 08:37:44 +0800

    Update EmployeeServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/EmployeeServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/EmployeeServiceImpl.java
@@ -6,10 +6,6 @@ import edu.czjt.reggie.entity.Employee;
 import edu.czjt.reggie.mapper.EmployeeMapper;
 import edu.czjt.reggie.service.EmployeeService;
 import org.springframework.stereotype.Service;
-
-/**
- * Created by jinkun.tian on 2023/3/16
- */
 @Service
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
 }
