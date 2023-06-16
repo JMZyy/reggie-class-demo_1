63ac90a95f5de08b735c1202863914f986642a2f
YH011229 <136041942+YH011229@users.noreply.github.com>
2023-06-16 08:36:53 +0800

    Update CategoryServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/CategoryServiceImpl.java
@@ -5,10 +5,5 @@ import edu.czjt.reggie.entity.Category;
 import edu.czjt.reggie.mapper.CategoryMapper;
 import edu.czjt.reggie.service.CategoryService;
 import org.springframework.stereotype.Service;
-
-/**
- * Created by jinkun.tian on 2023/4/27
- */
-@Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 }
